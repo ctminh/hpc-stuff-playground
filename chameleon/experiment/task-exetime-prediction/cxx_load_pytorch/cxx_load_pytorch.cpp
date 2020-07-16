39fedf238dd69189c113779fd66dfabadc088bac
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-07-16 10:32:20 +0200

    try to load the python model

--- chameleon/experiment/task-exetime-prediction/cxx_load_pytorch/cxx_load_pytorch.cpp
+++ chameleon/experiment/task-exetime-prediction/cxx_load_pytorch/cxx_load_pytorch.cpp
@@ -23,7 +23,7 @@ int main(int argc, const char* argv[]) {
 
   // Execute the model and turn its output into a tensor.
   at::Tensor output = module.forward(inputs).toTensor();
-  std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
+  std::cout << output << '\n';
 
   std::cout << "ok\n";
 }
