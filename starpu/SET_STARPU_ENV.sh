fdd5cdae47c979b558af915d0e5e9b30824b4ca4
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-07-20 17:20:38 +0200

    add script to configure starpu env

---
+++ starpu/SET_STARPU_ENV.sh
@@ -0,0 +1,3 @@
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/ra56kop/starpu-1.2.10/install/lib/pkgconfig 
+export LD_LIBRARY_PATH=/home/ra56kop/starpu-1.2.10/install/lib:$LD_LIBRARY_PATH
+export PATH=$PATH:/home/ra56kop/starpu-1.2.10/install/bin
