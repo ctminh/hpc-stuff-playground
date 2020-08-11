68a79d1d9e281c3ccc643eff500039e3abaaa379
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-08-11 18:48:12 +0200

    update the code simple_cuda

--- others/cuda_programming/simple_prog/simple_cuda.cu
+++ others/cuda_programming/simple_prog/simple_cuda.cu
@@ -1,62 +1,91 @@
 #include <iostream>
 #include <math.h>
+// For the CUDA runtime routines (prefixed with "cuda_")
+#include <cuda_runtime.h>
+#include <helper_cuda.h>
 
 // function to add elements of 2 arrays
 // CUDA Kernel function to add the elements of two arrays on the GPU
-__global__
-void add(int n, float *x, float *y)
+__global__ void
+vectorAdd(const float *A, const float *B, float *C, int numElements)
 {
-    for (int i = 0; i < n; i++)
-        y[i] = x[i] + y[i];
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i < numElements)
+    {
+        C[i] = A[i] + B[i];
+    }
 }
 
 // main function
 int main(void)
 {
-    int N = 1 << 20; // 1 milion elements
+    // Error code to check return values for CUDA calls
+    cudaError_t err = cudaSuccess;
 
-    // allocate arrays on CPU
-    float *x, *y;
-    float *gpu_x, *gpu_y;
-    x = (float *)malloc(sizeof(float) * N);
-    y = (float *)malloc(sizeof(float) * N);
+    // Print the vector length to be used, and compute its size
+    int numElements = 50000;
+    size_t size = numElements * sizeof(float);
+    printf("[Vector addition of %d elements]\n", numElements);
 
-    // initialize x and y
-    std::cout << "1. Init array x, y" << std::endl;
-    for (int i = 0; i < N; i++){
-        x[i] = 1.0f;
-        y[i] = 2.0f;
+    // Allocate the host input vector A, B, C
+    float *h_A = (float *)malloc(size);
+    float *h_B = (float *)malloc(size);
+    float *h_C = (float *)malloc(size);
+
+    // Initialize the host input vectors
+    for (int i = 0; i < numElements; ++i)
+    {
+        h_A[i] = rand()/(float)RAND_MAX;
+        h_B[i] = rand()/(float)RAND_MAX;
     }
 
-    // allocate mem on GPU
-    cudaMalloc((void **)&gpu_x, sizeof(float) * N);
-    cudaMalloc((void **)&gpu_y, sizeof(float) * N);
-    
-    // transfer data
-    cudaMemcpy(gpu_x, x, sizeof(float) * N, cudaMemcpyHostToDevice);
-    cudaMemcpy(gpu_y, y, sizeof(float) * N, cudaMemcpyHostToDevice);
+    // Allocate the device input vector A, B, C
+    float *d_A = NULL;
+    err = cudaMalloc((void **)&d_A, size);
+    float *d_B = NULL;
+    err = cudaMalloc((void **)&d_B, size);
+    float *d_C = NULL;
+    err = cudaMalloc((void **)&d_C, size);
 
-    // run add kernel on GPU
-    std::cout << "2. Run add kernel on GPU" << std::endl;
-    add<<<1, 1>>>(N, x, y);
+    // Copy the host input vectors A and B in host memory to the device input vectors in device memory
+    printf("Copy input data from the host memory to the CUDA device\n");
+    err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 
-    // synchronization: wait for GPU finishes
-    cudaDeviceSynchronize();
+    // Launch the Vector Add CUDA Kernel
+    int threadsPerBlock = 256;
+    int blocksPerGrid =(numElements + threadsPerBlock - 1) / threadsPerBlock;
+    printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
+    vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
+    err = cudaGetLastError();
 
-    // check the first 10 elements
-    for (int i = 0; i < 10; i++)
-        std::cout << y[i] << ",";
-    std::cout << std::endl;
+    // Copy the device result vector in device memory to the host result vector in host memory.
+    printf("Copy output data from the CUDA device to the host memory\n");
+    err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
 
-    // Check for errors (all values should be 3.0f)
-    float maxError = 0.0f;
-    for (int i = 0; i < N; i++)
-        maxError = fmax(maxError, fabs(y[i]-3.0f));
-    std::cout << "Max error: " << maxError << std::endl;
+    // Verify that the result vector is correct
+    for (int i = 0; i < numElements; ++i)
+    {
+        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5)
+        {
+            fprintf(stderr, "Result verification failed at element %d!\n", i);
+            exit(EXIT_FAILURE);
+        }
+    }
 
-    // Free mem on GPU
-    cudaFree(x);
-    cudaFree(y);
+    printf("Test PASSED\n");
 
+    // Free device global memory
+    err = cudaFree(d_A);
+    err = cudaFree(d_B);
+    err = cudaFree(d_C);
+
+    // Free host memory
+    free(h_A);
+    free(h_B);
+    free(h_C);
+
+    printf("Done\n");
     return 0;
 }
