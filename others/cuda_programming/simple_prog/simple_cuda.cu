6215d81fc36de4df39275d89020323a255981960
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-08-13 14:52:31 +0200

    debug the error on GPU

--- others/cuda_programming/simple_prog/simple_cuda.cu
+++ others/cuda_programming/simple_prog/simple_cuda.cu
@@ -43,15 +43,37 @@ int main(void)
     // Allocate the device input vector A, B, C
     float *d_A = NULL;
     err = cudaMalloc((void **)&d_A, size);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to allocate device vector A (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
     float *d_B = NULL;
     err = cudaMalloc((void **)&d_B, size);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to allocate device vector B (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
     float *d_C = NULL;
     err = cudaMalloc((void **)&d_C, size);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to allocate device vector C (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 
     // Copy the host input vectors A and B in host memory to the device input vectors in device memory
     printf("Copy input data from the host memory to the CUDA device\n");
     err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to copy vector A from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
     err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
+    }
 
     // Launch the Vector Add CUDA Kernel
     int threadsPerBlock = 1;
@@ -60,10 +82,18 @@ int main(void)
     printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
     vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
     err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 
     // Copy the device result vector in device memory to the host result vector in host memory.
     printf("Copy output data from the CUDA device to the host memory\n");
     err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess){
+        fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
 
     // Verify that the result vector is correct
     for (int i = 0; i < numElements; ++i)
