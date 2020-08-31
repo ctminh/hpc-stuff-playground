ab4812926f7b1737a7995a80d873b4f9ac8874a5
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-08-31 15:33:25 +0200

    update the ENV script on MNM-GPU-PC

--- starpu/SET_STARPU_ENV.sh
+++ starpu/SET_STARPU_ENV.sh
@@ -1,3 +1,6 @@
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/ra56kop/starpu-1.2.10/install/lib/pkgconfig 
-export LD_LIBRARY_PATH=/home/ra56kop/starpu-1.2.10/install/lib:$LD_LIBRARY_PATH
-export PATH=$PATH:/home/ra56kop/starpu-1.2.10/install/bin
+# export for GPU-PC at MNM-Chair
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/home/ctminh/Projects/starpu/starpu-1.2.10/install/lib/pkgconfig 
+export LD_LIBRARY_PATH=/home/ctminh/Projects/starpu/starpu-1.2.10/install/lib:$LD_LIBRARY_PATH
+export PATH=$PATH:/home/ctminh/Projects/starpu/starpu-1.2.10/install/bin
+export INCLUDE=/home/ctminh/Projects/starpu/starpu-1.2.10/install/include/starpu/1.2:$INCLUDE
+export CPATH=/home/ctminh/Projects/starpu/starpu-1.2.10/install/include/starpu/1.2:$CPATH
