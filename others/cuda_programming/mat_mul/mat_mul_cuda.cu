f67f3f257ab90c92f1a8eb0e6102b71e0d65304b
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-09-02 23:02:29 +0200

    update mxm with cuda

--- others/cuda_programming/mat_mul/mat_mul_cuda.cu
+++ others/cuda_programming/mat_mul/mat_mul_cuda.cu
@@ -23,10 +23,57 @@
 #include <helper_functions.h>
 #include <helper_cuda.h>
 
+
+/**
+ * Run a simple test of matrix multiplication using CUDA
+ */
+int MatrixMultiply(int argc, char **argv, int block_size, const dim3 &dimsA, const dim3 &dimsB) {
+    // allocate host memory for matrix A, B
+    unsigned int size_A = dimsA.x * dimsA.y;
+    unsigned int mem_size_A = sizeof(float) * size_A;
+    float *h_A = reinterpret_cast<float *>(malloc(mem_size_A));
+    unsigned int size_B = dimsB.x * dimsB.y;
+    unsigned int mem_size_B = sizeof(float) * size_B;
+    float *h_B = reinterpret_cast<float *>(malloc(mem_size_B));
+    cudaStream_t stream;
+
+    // initialize host memory
+    const float valA = 1.0f;
+    const float valB = 0.01f;
+    ConstantInit(h_A, size_A, valA);
+    ConstantInit(h_B, size_B, valB);
+
+    // allocate device memory
+    float *d_A, *d_B, *d_C;
+    dim3 dimsC(dimsB.x, dimsA.y, 1);
+    unsigned int mem_size_C = dimsC.x * dimsC.y * sizeof(float);
+    float *h_C = reinterpret_cast<float *>(malloc(mem_size_C));
+    if (h_C == NULL) {
+        fprintf(stderr, "Failed to allocate host matrix C!\n");
+        exit(EXIT_FAILURE);
+    }
+    // check mem allocation
+    checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&d_A), mem_size_A));
+    checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&d_B), mem_size_B));
+    checkCudaErrors(cudaMalloc(reinterpret_cast<void **>(&d_C), mem_size_C));
+
+    // allocate cuda events that we will use for timing
+    cudaEvent_t start, stop;
+    checkCudaErrors(cudaEventCreate(&start));
+    checkCudaErrors(cudaEventCreate(&stop));
+    checkCudaErrors(cudaStreamCreateWithFlags(&stream, cudaStreamNonBlocking));
+
+    // copy host mem to device
+    checkCudaErrors(cudaMemcpyAsync(d_A, h_A, mem_size_A, cudaMemcpyHostToDevice, stream));
+    checkCudaErrors(cudaMemcpyAsync(d_B, h_B, mem_size_B, cudaMemcpyHostToDevice, stream));
+
+}
+
+
 /**
  * Program main
  */
- int main(int argc, char **argv) {
+int main(int argc, char **argv) {
     printf("[Matrix Multiply Using CUDA] - Starting...\n");
     if (checkCmdLineFlag(argc, (const char **)argv, "help") || checkCmdLineFlag(argc, (const char **)argv, "?")) {
         printf("Usage -device=n (n >= 0 for deviceID)\n");
@@ -39,6 +86,38 @@
     // This will pick the best possible CUDA capable device, otherwise
     // override the device ID based on input provided at the command line
     int dev = findCudaDevice(argc, (const char **)argv);
+
+    // declare the matrix
+    int block_size = 32;
+    dim3 dimsA(5 * 2 * block_size, 5 * 2 * block_size, 1);
+    dim3 dimsB(5 * 4 * block_size, 5 * 2 * block_size, 1);
+    // get width of Matrix A
+    if (checkCmdLineFlag(argc, (const char **)argv, "wA")){
+        dimsA.x = getCmdLineArgumentInc(argc, (const char **)argv, "wA");
+    }
+    // get height of Matrix A
+    if (checkCmdLineFlag(argc, (const char **)argv, "hA")){
+        dimsA.y = getCmdLineArgumentInc(argc, (const char **)argv, "hA");
+    }
+    // get height of Matrix B
+    if (checkCmdLineFlag(argc, (const char **)argv, "wB")){
+        dimsB.x = getCmdLineArgumentInc(argc, (const char **)argv, "wB");
+    }
+    // get height of Matrix B
+    if (checkCmdLineFlag(argc, (const char **)argv, "hB")){
+        dimsB.y = getCmdLineArgumentInc(argc, (const char **)argv, "hB");
+    }
+    // check the size of A & B
+    if (dimsA.x != dimsB.y){
+        printf("Error: outer matrix dimensions must be equal. (%d != %d)\n", dimsA.x, dimsB.y);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("MatrixA(%d,%d), MatrixB(%d,%d)\n", dimsA.x, dimsA.y, dimsB.x, dimsB.y);
+    
+    int matrix_result = MatrixMultiply(argc, argv, block_size, dimsA, dimsB);
+
+    exit(matrix_result);
 
     return 0;
 }
