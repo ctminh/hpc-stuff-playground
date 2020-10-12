5a47ec69913f6acccc00803230a327a6e3e72670
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-10-12 14:14:41 +0200

    update exporting env

--- starpu/SET_STARPU_ENV.sh
+++ starpu/SET_STARPU_ENV.sh
@@ -2,7 +2,7 @@
 if [ "${EXPORT_MNM_GPU_PC}" = "1" ]
 then
 export STARTPU_HOME=/home/ctminh/Projects/starpu/starpu-1.2.10/install
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$STARTPU_HOME/lib/pkgconfig 
+export PKG_CONFIG_PATH=$STARTPU_HOME/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=$STARTPU_HOME/lib:$LD_LIBRARY_PATH
 export PATH=$PATH:$STARTPU_HOME/bin
 export INCLUDE=$STARTPU_HOME/include/starpu/1.2:$INCLUDE
