84b957598653fbeb12751a868bb0865eabef986c
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-10-13 16:57:23 +0200

    update a stencil example

--- starpu/stencil_gameoflife/stencil.c
+++ starpu/stencil_gameoflife/stencil.c
@@ -1,19 +1,15 @@
 #include "stencil.h"
-#include <stdio.h>
 
-// #ifdef STARPU_HAVE_VALGRIND_H
-// #include <valgrind/valgrind.h>
-// #endif
 
-// #ifdef STARPU_QUICK_CHECK
-// static unsigned niter = 4;
-// #define SIZE 16
-// #define NBZ 8
-// #else
+#ifdef STARPU_QUICK_CHECK
+static unsigned niter = 4;
+#define SIZE 16
+#define NBZ 8
+#else
 static unsigned niter = 32;
 #define SIZE 128
 #define NBZ 64
-// #endif
+#endif
 
 /* default parameter values */
 static unsigned bind_tasks = 0;
