0c524a649bcc97b2295b11a8e96f6d96e000b6a2
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-10-13 15:55:45 +0200

    update a stencil example

---
+++ starpu/stencil_gameoflife/stencil.c
@@ -0,0 +1,74 @@
+#include "stencil.h"
+
+#ifdef STARPU_HAVE_VALGRIND_H
+#include <valgrind/valgrind.h>
+#endif
+
+/* Main application */
+
+/* default parameter values */
+static unsigned  bind_tasks = 0;
+
+static unsigned ticks = 1000;
+
+#ifdef STARPU_QUICK_CHECK
+static unsigned niter = 4;
+#define SIZE 16
+#define NBZ 8
+#else
+static unsigned niter = 32;
+#define SIZE 128
+#define NBZ 64
+#endif
+
+/* Problem size */
+static unsigned sizex = SIZE;
+static unsigned sizey = SIZE;
+static unsigned sizez = NBZ*SIZE;
+
+/* Number of blocks (scattered over the different MPI processes) */
+unsigned nbz = NBZ;
+
+/* Initialization */
+
+
+int main(int argc, char **argv)
+{
+    int rank;
+    int world_size;
+    int ret;
+
+    // STARPU USE MPI
+    printf("[CHECK] STARPU_USE_MPI=%d\n", STARPU_USE_MPI);
+    #if define(STARPU_USE_MPI)
+        int thread_support;
+        if (MPI_Init_thread(&argc, &argv, MPI_THREAD_SERIALIZED, &thread_support)){
+            FPRINTF(stderr, "MPI_Init_thread failed\n");
+        }
+
+        if (thread_support == MPI_THREAD_FUNNELED)
+		    FPRINTF(stderr,"Warning: MPI only has funneled thread support, not serialized, hoping this will work\n");
+
+	    if (thread_support < MPI_THREAD_FUNNELED)
+		    FPRINTF(stderr,"Warning: MPI does not have thread support!\n");
+
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    #else
+        rank = 0;
+        world_size = 1;
+    #endif
+
+    if (rank == 0){
+        FPRINTF(stderr, "Running on %d nodes\n", world_size);
+		fflush(stderr);
+    }
+
+    // init starpu
+    printf("1. init StarPU ...")
+    ret = starpu_init(NULL);
+	if (ret == -ENODEV) return 77;
+	STARPU_CHECK_RETURN_VALUE(ret, "starpu_init");
+
+    return 0;
+}
---
+++ starpu/stencil_gameoflife/stencil.h
@@ -0,0 +1,64 @@
+#ifndef __STENCIL_H__
+#define __STENCIL_H__
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <starpu.h>
+
+// if define CUDA
+#ifndef __CUDACC__
+#if defined(STARPU_USE_MPI) && !defined(STARPU_USE_MPI_MASTER_SLAVE)
+#include <mpi.h>
+#include <starpu_mpi.h>
+#endif
+#endif
+
+// define error display
+#define FPRINTF(ofile, fmt, ...) do { if (!getenv("STARPU_SSILENT")) {fprintf(ofile, fmt, ## __VA_ARGS__); }} while(0)
+
+// define life
+#define LIFE
+
+#ifdef LIFE
+#define TYPE	unsigned char
+extern void life_update(int bz, const TYPE *old, TYPE *newp, int nx, int ny, int nz, int ldy, int ldz, int iter);
+#else
+#define TYPE	float
+#endif
+
+#define K	1   // what is K???
+
+#define NDIRS 2 // what is NDIRs???
+
+/* Split only on the z axis to make things simple */
+typedef enum
+{
+	B = 0,
+	T = 1
+} direction;
+
+/* description of a domain block */
+struct block_description
+{
+    /* which MPI node should process that block? */
+    int mpi_node;
+    unsigned perferred_worker;
+    unsigned bz;
+
+    /* For each of the following buffers, there are two (0/1) buffers to
+	* make new/old switch costless. */
+    
+    /* This is the computation buffer for this block, it includes
+	* neighbours' border to make computation easier */
+    TYPE *layers[2];
+	starpu_data_handle_t layers_handle[2];
+
+    /* This is the "save" buffer, i.e. a copy of our neighbour's border.
+	 * This one is used for CPU/GPU or MPI communication (rather than the
+	 * whole domain block) */
+	TYPE *boundaries[NDIRS][2];
+	starpu_data_handle_t boundaries_handle[NDIRS][2];
+
+	/* Shortcut pointer to the neighbours */
+	struct block_description *boundary_blocks[NDIRS];
+}
