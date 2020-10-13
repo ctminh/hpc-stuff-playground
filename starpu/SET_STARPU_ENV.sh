03e22d68c50be296135cc8ad279b69243c30b5d9
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-10-13 16:18:04 +0200

    update a stencil example

--- starpu/SET_STARPU_ENV.sh
+++ starpu/SET_STARPU_ENV.sh
@@ -2,10 +2,11 @@
 OPENCL_INC=/usr/local/cuda/include
 OPENCL_LIB=/usr/local/cuda/lib64
 HWLOC_HOME=/home/ctminh/Projects/loc-libs/hwloc/hwloc-2.2.0/build
+INTEL_MPI=/home/ctminh/intel/compilers_and_libraries_2020.2.254/linux/mpi/intel64
 export PKG_CONFIG_PATH=$HWLOC_HOME/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=$OPENCL_LIB:$HWLOC_HOME/lib:$LD_LIBRARY_PATH
-export INCLUDE=$OPENCL_INC:$HWLOC_HOME/include:$INCLUDE
-export CPATH=$OPENCL_INC:$HWLOC_HOME/include:$CPATH
+export INCLUDE=$OPENCL_INC:$HWLOC_HOME/include:$INTEL_MPI/include:$INCLUDE
+export CPATH=$OPENCL_INC:$HWLOC_HOME/include:$INTEL_MPI/include:$CPATH
 
 if [ "${EXPORT_MNM_GPU_PC}" = "1" ]
 then
