77ce528767ce5004cd5fda382ad058c5ce82f33d
Thanh Chung <minh.thanh.chung@nm.ifi.lmu.de>
2020-10-14 17:23:44 +0200

    added new env_exp

--- starpu/SET_STARPU_ENV.sh
+++ starpu/SET_STARPU_ENV.sh
@@ -3,10 +3,15 @@ OPENCL_INC=/usr/local/cuda/include
 OPENCL_LIB=/usr/local/cuda/lib64
 HWLOC_HOME=/home/ctminh/Projects/loc-libs/hwloc/hwloc-2.2.0/build
 INTEL_MPI=/home/ctminh/intel/compilers_and_libraries_2020.2.254/linux/mpi/intel64
+
+echo "exporting ENV vars of hwloc, opencl, intel_comp_inc paths"
 export PKG_CONFIG_PATH=$HWLOC_HOME/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=$OPENCL_LIB:$HWLOC_HOME/lib:$LD_LIBRARY_PATH
 export INCLUDE=$OPENCL_INC:$HWLOC_HOME/include:$INTEL_MPI/include:$INCLUDE
 export CPATH=$OPENCL_INC:$HWLOC_HOME/include:$INTEL_MPI/include:$CPATH
+
+echo "exporting intel mpi envs"
+source /home/ctminh/intel/bin/compilervars.sh intel64
 
 echo "export ENV_VARs with EXPORT_MNM_GPU_PC = ${EXPORT_MNM_GPU_PC}"
 
