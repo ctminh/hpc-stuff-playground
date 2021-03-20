e05f12d06b36931ff3287a0eb24db6d92d8c61b7
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-03-20 23:13:01 +0100

    added another example of margo with rpc-hello

---
+++ others/margo_rpc/hello-rpc/rpc-client.c
@@ -0,0 +1,55 @@
+#include <assert.h>
+#include <stdio.h>
+#include <margo.h>
+
+int main(int argc, char** argv)
+{
+    /* This client takes the server’s address as argument (copy-past the
+    address printed by the server when calling the client). This string
+    representation of the server’s address must be resolved into a hg_addr_t
+    object. This is done by margo_addr_lookup. */
+    if(argc != 2) {
+        fprintf(stderr,"Usage: %s <server address>\n", argv[0]);
+        exit(0);
+    }
+
+    // init margo-client
+    margo_instance_id mid = MARGO_INSTANCE_NULL;
+    mid = margo_init("tcp", MARGO_CLIENT_MODE, 0, 0);
+    assert(mid);
+
+    // register the function to be called at the server side
+    hg_id_t hello_rpc_id = MARGO_REGISTER(mid, "hello", void, void, NULL);
+    margo_registered_disable_response(mid, hello_rpc_id, HG_TRUE);
+
+    // Once resolved, the address can be used in a call to margo_create to create a
+    // hg_handle_t object. The hg_handle_t object represents an RPC request ready to
+    // be sent to the server.
+    hg_return_t ret;
+    hg_addr_t svr_addr;
+    ret = margo_addr_lookup(mid, argv[1], &svr_addr);
+    assert(ret == HG_SUCCESS);
+
+    hg_handle_t handle;
+    ret = margo_create(mid, svr_addr, hello_rpc_id, &handle);
+    assert(ret == HG_SUCCESS);
+
+    // margo_forward effectively sends the request to the server.
+    // We pass NULL as a second argument because the RPC does not take any input.
+    ret = margo_forward(handle, NULL);
+    assert(ret == HG_SUCCESS);
+
+    /* Because we have called margo_registered_disable_response, Margo knows that
+    the client should not expect a response from the server, hence margo_forward will
+    return immediately. We then destroy the handle using margo_destroy, free the hg_addr_t
+    object using margo_addr_free, and finalize Margo. */
+    ret = margo_destroy(handle);
+    assert(ret == HG_SUCCESS);
+
+    ret = margo_addr_free(mid, svr_addr);
+    assert(ret == HG_SUCCESS);
+
+    margo_finalize(mid);
+
+    return 0;
+}
---
+++ others/margo_rpc/hello-rpc/rpc-server.c
@@ -0,0 +1,72 @@
+#include <assert.h>
+#include <stdio.h>
+#include <margo.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+// declare num total rpc requests
+static const int TOTAL_RPCS = 4;
+static int num_rpcs = 0;
+
+// declare the function would be called
+// DECLARE_MARGO_RPC_HANDLER is a macro that generates the code
+// necessary for the RPC handler to be placed in an Argobots user-level thread.
+static void hello_world(hg_handle_t h);
+DECLARE_MARGO_RPC_HANDLER(hello_world)
+
+int main(int argc, char** argv)
+{
+    margo_instance_id mid = margo_init("tcp", MARGO_SERVER_MODE, 0, -1);
+    assert(mid);
+
+    hg_addr_t my_address;
+    margo_addr_self(mid, &my_address);
+    char addr_str[128];
+    size_t addr_str_size = 128;
+    margo_addr_to_string(mid, addr_str, &addr_str_size, my_address);
+    margo_addr_free(mid,my_address);
+
+    margo_set_log_level(mid, MARGO_LOG_INFO);
+    pid_t s_pid = getpid();
+    margo_info(mid, "[SERVER] pid=%d is running at address %s", s_pid, addr_str);
+
+    // The two lines that register the RPC handler in the Margo instance are the following
+    //  + MARGO_REGISTER is a macro that registers the RPC handler. Its first argument is the
+    //    Margo instance. The second is the name of the RPC. The third and fourth are the
+    //    types of the RPC’s input and output, respectively. The last parameter is the function
+    //    we want to use as RPC handler
+    //  + margo_registered_disable_response is used to indicate that this RPC handler does not
+    //    send a response back to the client.
+    hg_id_t rpc_id = MARGO_REGISTER(mid, "hello", void, void, hello_world);
+    margo_registered_disable_response(mid, rpc_id, HG_TRUE);
+
+    margo_wait_for_finalize(mid);
+
+    return 0;
+}
+
+/* The rest of the program defines the hello_world function.
+From inside an RPC handler, we can access the Margo instance using margo_hg_handle_get_instance.
+This is the prefered method for better code organization, rather than declaring the Margo instance
+as a global variable. The RPC handler must call margo_destroy on the hg_handle_t argument it is
+being passed, after we are done using it.
+    - The question is: how to pass h??? Maybe pass it at the client side...
+*/
+static void hello_world(hg_handle_t h)
+{
+    hg_return_t ret;
+
+    margo_instance_id mid = margo_hg_handle_get_instance(h);
+
+    margo_info(mid, "Hello World!");
+    num_rpcs += 1;
+
+    ret = margo_destroy(h);
+    assert(ret == HG_SUCCESS);
+
+    if(num_rpcs == TOTAL_RPCS) {
+        margo_finalize(mid);
+    }
+}
+
+DEFINE_MARGO_RPC_HANDLER(hello_world)
