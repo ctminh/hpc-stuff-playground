31054709d99f3e36c2a781d500dea78150df0b5f
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-03-31 23:36:23 +0200

    updated slurm-script for running rpc-hello

--- others/margo_rpc/hello-rpc/build/submit_job_coolmuc.cmd
+++ others/margo_rpc/hello-rpc/build/submit_job_coolmuc.cmd
@@ -52,8 +52,8 @@ sleep 5
 echo "3. [BASH-SCRIPT] Reading input_file..."
 cur_dir=$(pwd)
 input_file=$(<${cur_dir}/f_server_addr.txt)
-IFS=\/ read -a fields <<< $input_file ##"$ADDRESS"
-IFS=   read -a w_addr <<< $input_file
+IFS=\/ read -a fields <<< $input_file
+IFS=   read -a s_addr <<< $input_file
 
 ## reset IFS back the default
 ## set | grep ^IFS=
@@ -63,19 +63,19 @@ IFS=   read -a w_addr <<< $input_file
 ## set | grep ^fields=\\\|^IN=
 
 ## e.g., fields=([0]="ofi+tcp" [1]="ofi_rxm://10.7.5.34:35271")
-echo "    fields[0] = ${fields[0]}"
-echo "    fields[1] = ${fields[1]}"
-echo "    fields[2] = ${fields[2]}"
+## echo "    fields[0] = ${fields[0]}"
+## echo "    fields[1] = ${fields[1]}"
+## echo "    fields[2] = ${fields[2]}"
 
 ## seperate IP_addr and Port_number
 IFS=\: read -a ser_addr <<< ${fields[2]}
-echo "    IP_addr=${ser_addr[0]} | Port=${ser_addr[1]}"
+echo "    Server_IP_Addr=${ser_addr[0]} | Port=${ser_addr[1]}"
 
 ## -----------------------------------------
 ## -------- Running clients ----------------
-echo "4. Running client..."
+echo "4. Running client (just work with the ser_addr format)..."
 echo "    mpirun -n 1 --host ${node_arr[1]} ./rpc_client ${w_addr}"
-mpirun -n 1 --host ${node_arr[1]} ./rpc_client ${w_addr}
+mpirun -n 1 --host ${node_arr[1]} ./rpc_client ${s_addr}
 
 echo "Done!"
 rm ./nodelist.txt
