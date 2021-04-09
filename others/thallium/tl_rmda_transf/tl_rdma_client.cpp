307487b8ccd33454ed1dfe3eb14e988431756abd
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-04-09 15:30:05 +0200

    added an example of tl transfering data with rdma

---
+++ others/thallium/tl_rmda_transf/tl_rdma_client.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thallium.hpp>
+
+namespace tl = thallium;
+
+int main(int argc, char** argv) {
+    // init the tl-client mode
+    tl::engine myEngine("tcp", MARGO_CLIENT_MODE);
+    tl::remote_procedure remote_do_rdma = myEngine.define("do_rdma").disable_response();
+    tl::endpoint server_endpoint = myEngine.lookup(argv[1]);
+
+    // we define a buffer with the content “Matthieu” (because it’s a string,
+    // there is actually a null-terminating character). We then define
+    // segments as a vector of pairs of void* and std::size_t
+    std::string buffer = "Matthieu";
+    std::vector<std::pair<void*, std::size_t>> segments(1);
+
+    // Each segment (here only one) is characterized by its starting
+    // address in local memory and its size. 
+    segments[0].first  = (void*)(&buffer[0]);
+    segments[0].second = buffer.size()+1;
+
+    // We call engine::expose to expose the buffer and get a bulk instance from it.
+    // We specify tl::bulk_mode::read_only to indicate that the memory will only be
+    // read by other processes (alternatives are tl::bulk_mode::read_write
+    // and tl::bulk_mode::write_only). 
+    tl::bulk myBulk = myEngine.expose(segments, tl::bulk_mode::read_only);
+
+    // Finally we send an RPC to the server, passing the bulk object as an argument.
+    remote_do_rdma.on(server_endpoint)(myBulk);
+
+    return 0;
+}
