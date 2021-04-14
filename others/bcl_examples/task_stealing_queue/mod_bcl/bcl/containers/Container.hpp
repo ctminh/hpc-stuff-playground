bee8a44b383c13519d0d5c0dc9dc69604cd724bf
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-04-14 12:09:30 +0200

    updated a small change from Nikola

--- others/bcl_examples/task_stealing_queue/mod_bcl/bcl/containers/Container.hpp
+++ others/bcl_examples/task_stealing_queue/mod_bcl/bcl/containers/Container.hpp
@@ -7,6 +7,7 @@
 #include <math.h>
 
 #include <bcl/bcl.hpp>
+int alloced = 0;
 
 namespace BCL {
   template <typename T, size_t N>
@@ -100,33 +101,48 @@ namespace BCL {
   struct serialize <task> {
     serial_ptr <double> operator()(const task &task) const noexcept {
       serial_ptr <double> ptr;
-      ptr.N = task.matrixSize*task.matrixSize*2;
+      ptr.N = task.matrixSize*task.matrixSize*2 + 1;
       ptr.ptr = std::shared_ptr <double> (new double[ptr.N]);
-      for (int i = 0; i < ptr.N/2; i++) {
+
+      ptr.ptr.get()[ptr.N-1] = task.matrixSize;
+
+      for (int i = 0; i < (ptr.N-1)/2; i++) {
         ptr.ptr.get()[i] = task.matrix[i];
       }
-      for(int i = ptr.N/2; i< ptr.N; i++){
+      for(int i = (ptr.N-1)/2; i< ptr.N-1; i++){
         ptr.ptr.get()[i] = task.matrix2[i-ptr.N/2];
       }
       // printf("[%ld]Freed %d\n", BCL::rank(), task.matrixSize*task.matrixSize*2);
       free(task.matrix);
       free(task.matrix2);
+      
+      // alloced-=2;
+      // if(BCL::rank()==0)
+      //   printf("[%ld](s)Alloced container: %d\n", BCL::rank(),alloced);
+      
       return ptr;
     }
+    
     task deserialize(const serial_ptr <double> &ptr) const noexcept {
       task t;
-      t.matrixSize = sqrt(ptr.N/2);
+      double* matrices = ptr.ptr.get();
+      t.matrixSize = matrices[ptr.N-1];
       t.matrix = (double*)malloc(sizeof(double) * t.matrixSize * t.matrixSize);
       t.matrix2 = (double*)malloc(sizeof(double) * t.matrixSize * t.matrixSize);
       // printf("[%ld]Alloced %d\n", BCL::rank(), t.matrixSize*t.matrixSize*2);
       
       for(int i = 0; i<t.matrixSize*t.matrixSize; i++){
-        t.matrix[i] = ptr.ptr.get()[i];
+        t.matrix[i] = matrices[i];
         // printf("Matrix[%d]: %lf\n",i,t.matrix[i]);
       }
       for(int i = 0; i<t.matrixSize*t.matrixSize; i++){
-        t.matrix2[i] = ptr.ptr.get()[i+t.matrixSize*t.matrixSize];
+        t.matrix2[i] = matrices[i+t.matrixSize*t.matrixSize];
       }
+      // alloced+=2;
+      
+      // if(BCL::rank()==0)
+      //   printf("[%ld](des)Alloced container: %d\n", BCL::rank(),alloced);
+
       return t;
     }
   };
@@ -158,37 +174,7 @@ namespace BCL {
     }
   };
 
-  template <size_t N>
-  struct serialize <task, N, void> {
-    serial_blob <double, N> operator()(const task &t) const noexcept {
-      serial_blob <double, N> blob;
 
-      for (int i = 0; i < t.matrixSize * t.matrixSize; i++) {
-        blob.val[i] = t.matrix[i];
-      }
-      for (int i = t.matrixSize*t.matrixSize; i < 2*t.matrixSize * t.matrixSize; i++) {
-        blob.val[i] = t.matrix2[i-t.matrixSize*t.matrixSize];
-      }
-      blob.len = t.matrixSize*t.matrixSize*2;
-      return blob;
-    }
-    task deserialize(const serial_blob <double, N> &blob) const noexcept {
-      task t;
-      t.matrixSize = sqrt(blob.len/2);
-      t.matrix = (double*)malloc(sizeof(double) * t.matrixSize * t.matrixSize);
-      t.matrix2 = (double*)malloc(sizeof(double) * t.matrixSize * t.matrixSize);
-      // printf("[%ld]Alloced %d\n", BCL::rank(), t.matrixSize*t.matrixSize*2);
-      
-      for(int i = 0; i<t.matrixSize*t.matrixSize; i++){
-        t.matrix[i] = blob.val[i];
-        // printf("Matrix[%d]: %lf\n",i,t.matrix[i]);
-      }
-      for(int i = 0; i<t.matrixSize*t.matrixSize; i++){
-        t.matrix2[i] = blob.val[i+t.matrixSize*t.matrixSize];
-      }
-      return t;
-    }
-  };
 
   template <typename T, typename Serialize, typename Enabler = void>
   class Container {
@@ -286,6 +272,54 @@ namespace BCL {
     void get() const {}
     void set() {}
   };
+
+  // template <>
+  // struct serialize <task> {
+  //   serial_ptr  <Container<double, serialize<double>>> operator()(const task &task) const noexcept {
+  //     serial_ptr <Container<double, serialize<double>>> ptr;
+  //     ptr.N = task.matrixSize*task.matrixSize*2 + 1;
+  //     ptr.ptr = std::shared_ptr <Container<double, serialize<double>>> (new Container<double, serialize<double>>[ptr.N]);
+
+  //     ptr.ptr.get()[ptr.N-1] = Container<double, serialize<double>> (task.matrixSize);
+
+  //     for (int i = 0; i < ptr.N/2; i++) {
+  //       ptr.ptr.get()[i] = Container<double, serialize<double>> (task.matrix[i]);
+  //     }
+  //     for(int i = ptr.N/2; i< ptr.N-1; i++){
+  //       ptr.ptr.get()[i] = Container<double, serialize<double>> (task.matrix2[i-ptr.N/2]);
+  //     }
+  //     // printf("[%ld]Freed %d\n", BCL::rank(), task.matrixSize*task.matrixSize*2);
+  //     free(task.matrix);
+  //     free(task.matrix2);
+      
+  //     alloced-=2;
+  //     // if(BCL::rank()==0)
+  //     //   printf("[%ld](s)Alloced container: %d\n", BCL::rank(),alloced);
+      
+  //     return ptr;
+  //   }
+  //   task deserialize(const serial_ptr <Container<double, serialize<double>>> &ptr) const noexcept {
+
+  //     task t;
+  //     t.matrixSize = ptr.ptr.get()[ptr.N-1].get();
+  //     t.matrix = (double*)malloc(sizeof(double) * t.matrixSize * t.matrixSize);
+  //     t.matrix2 = (double*)malloc(sizeof(double) * t.matrixSize * t.matrixSize);
+  //     // printf("[%ld]Alloced %d\n", BCL::rank(), t.matrixSize*t.matrixSize*2);
+      
+  //     for(int i = 0; i<t.matrixSize*t.matrixSize; i++){
+  //       t.matrix[i] = ptr.ptr.get()[i].get();
+  //       // printf("Matrix[%d]: %lf\n",i,t.matrix[i]);
+  //     }
+  //     for(int i = 0; i<t.matrixSize*t.matrixSize; i++){
+  //       t.matrix2[i] = ptr.ptr.get()[i+t.matrixSize*t.matrixSize].get();
+  //     }
+  //     alloced+=2;
+      
+  //     // if(BCL::rank()==0)
+  //     //   printf("[%ld](des)Alloced container: %d\n", BCL::rank(),alloced);
+  //     return t;
+  //   }
+  // };
 
   template <typename T>
   struct serialize <std::vector <T>> {
--- others/bcl_examples/task_stealing_queue/mod_bcl/examples/simple/matrix.cpp
+++ others/bcl_examples/task_stealing_queue/mod_bcl/examples/simple/matrix.cpp
@@ -8,7 +8,8 @@
 #include <random>
 
 double fTimeStart, fTimeEnd;
-bool taskStealing = true;
+bool taskStealing = false;
+int freed = 0;
 
 void multiply(double *matrix, double *matrix2, double *result, int matrixSize);
 void initialize_matrix_rnd(double *mat, int matrixSize);
@@ -26,66 +27,80 @@ int main(int argc, char **argv)
     BCL::init();
 
     int matrixSize = atoi(argv[1]);
-    int tasks = atoi(argv[BCL::rank() + 2]);
+    int tasks = 0;
+
+    //get the number of tasks to enqueue
+    int rankOffset = 0;
+    for (int i = 2; i < argc; i += 2)
+    {
+        if (atoi(argv[i + 1]) + rankOffset > BCL::rank() && rankOffset <= BCL::rank())
+        {
+            tasks = atoi(argv[i]);
+        }
+        rankOffset += atoi(argv[i + 1]);
+    }
 
     std::vector<BCL::CircularQueue<task>> queues;
     for (size_t rank = 0; rank < BCL::nprocs(); rank++)
     {
-
         queues.push_back(BCL::CircularQueue<task>(rank, 100000));
     }
 
     if (BCL::rank() == 0)
         fTimeStart = curtime();
-        // fTimeStart = MPI_Wtime();
 
     //create tasks
-    for (int i = 0; i < tasks; i++)
+    for (int it = 0; it < 5; it++)
     {
-        struct task t;
-        t.matrix = (double *)malloc(sizeof(double) * matrixSize * matrixSize);
-        t.matrix2 = (double *)malloc(sizeof(double) * matrixSize * matrixSize);
-        t.matrixSize = matrixSize;
-        initialize_matrix_rnd(t.matrix, matrixSize);
-        initialize_matrix_rnd(t.matrix2, matrixSize);
-        queues[BCL::rank()].push(t);
-    }
-
-    BCL::barrier();
-    //for (int i = 0; i < BCL::nprocs(); i++)
-    //    printf("[%ld]Rank %d:%ld\n", BCL::rank(), i, queues[i].size());
-
-    //solve tasks
-    while (true)
-    {
-        //steal tasks if activated
-        if (queues[BCL::rank()].empty())
+        for (int i = 0; i < tasks; i++)
         {
-            if (!taskStealing)
-                break;
-            if (!steal(&queues))
-            {
-                break;
-            }
+            struct task t;
+            t.matrix = (double *)malloc(sizeof(double) * matrixSize * matrixSize);
+            t.matrix2 = (double *)malloc(sizeof(double) * matrixSize * matrixSize);
+            t.matrixSize = matrixSize;
+            initialize_matrix_rnd(t.matrix, matrixSize);
+            initialize_matrix_rnd(t.matrix2, matrixSize);
+            queues[BCL::rank()].push(t, BCL::CircularQueueAL::push);
         }
 
-        task t;
-        queues[BCL::rank()].pop(t);
-        double *result = (double *)malloc(sizeof(double) * matrixSize * matrixSize);
-        multiply(t.matrix, t.matrix2, result, matrixSize);
-        if(BCL::rank() == 3)
-            printf("%ld\n",(queues)[BCL::rank()].size());
-        // printf("[%ld]Freed %d\n", BCL::rank(), t.matrixSize*t.matrixSize*2);
-        free(t.matrix);
-        free(t.matrix2);
-        free(result);
-        // int flag;
-        // MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, MPI_STATUS_IGNORE);
+
+        BCL::barrier();
+        // for (int i = 0; i < BCL::nprocs(); i++)
+        //    printf("[%ld]Rank %d:%ld\n", BCL::rank(), i, queues[i].size());
+
+        //solve tasks
+        while (true)
+        {
+            //steal tasks if activated
+            if (queues[BCL::rank()].empty())
+            {
+                if (!taskStealing)
+                    break;
+                if (!steal(&queues))
+                {
+                    break;
+                }
+            }
+
+            task t;
+            bool success = queues[BCL::rank()].pop(t);
+            if (success)
+            {
+                double *result = new double[matrixSize * matrixSize];
+                multiply(t.matrix, t.matrix2, result, matrixSize);
+                free(t.matrix);
+                free(t.matrix2);
+                delete[] result;
+                // freed += 2;
+                // if(BCL::rank()==0)
+                //     printf("[%ld]Freed matrix: %d\n", BCL::rank(),freed);
+            }
+        }
+        BCL::barrier();
     }
 
-    BCL::barrier();
-
-    if (BCL::rank() == 0){
+    if (BCL::rank() == 0)
+    {
         fTimeEnd = curtime();
         double elapsed = fTimeEnd - fTimeStart;
         printf("%lf\n", elapsed);
@@ -124,33 +139,39 @@ void multiply(double *matrix, double *matrix2, double *result, int matrixSize)
 
 bool steal(std::vector<BCL::CircularQueue<task>> *queues)
 {
+    std::srand(unsigned(std::time(0)));
+    std::vector<int> ranks;
+    for (int i = 0; i < BCL::nprocs(); ++i)
+    {
+        if (i != BCL::rank())
+            ranks.push_back(i);
+    }
+
+    std::random_shuffle(ranks.begin(), ranks.end());
+
     int i = (BCL::rank() + 1) % BCL::nprocs();
     //iterates through ranks and tests if they have tasks left
-    while (i != BCL::rank())
-    { //printf("[%ld]testing %d\n",BCL::rank(), i);
-        //printf("[%ld]Trying to steal from rank %d: size: %d\n", BCL::rank(), i, (*queues)[i].size());
-        if ((*queues)[i].size() > 1)
+    for (std::vector<int>::iterator it = ranks.begin(); it != ranks.end(); ++it)
+    {
+        // printf("[%ld]Current: %d\n", BCL::rank(), *it);
+        if ((*queues)[*it].size() > 1)
         {
             //printf("[%ld]found %d!\n", BCL::rank(),i);
             task t;
             //steals half the tasks
-            for (int j = 0; j < (*queues)[i].size() / 2; j++)
+            for (int j = 0; j < (*queues)[*it].size() / 2; j++)
             {
 
-                (*queues)[i].pop(t);
-                // printf("stealing task %d\n",j);
+                (*queues)[*it].pop(t, BCL::CircularQueueAL::pop);
+                //printf("[%ld]stealing task %d\n", BCL::rank(), j);
                 (*queues)[BCL::rank()].push(t);
             }
 
             if (!(*queues)[BCL::rank()].empty())
             {
-                printf("Successfully stolen %ld tasks!\n", (*queues)[BCL::rank()].size());
+                printf("[%ld]Successfully stolen %ld tasks!\n", BCL::rank(), (*queues)[BCL::rank()].size());
                 return true;
             }
-        }
-        else
-        {
-            i = (i + 1) % BCL::nprocs();
         }
     }
     return false;
