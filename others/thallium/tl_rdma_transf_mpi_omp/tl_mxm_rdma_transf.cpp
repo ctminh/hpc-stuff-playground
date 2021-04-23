1e0ddc99cb77fe7c34a255142c577b43ca104236
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-04-23 23:17:53 +0200

    updated full rpc-functions at server client sides

--- others/thallium/tl_rdma_transf_mpi_omp/tl_mxm_rdma_transf.cpp
+++ others/thallium/tl_rdma_transf_mpi_omp/tl_mxm_rdma_transf.cpp
@@ -116,9 +116,42 @@ int main(int argc, char **argv){
         std::string str_serveraddr = ser_engine.self();
         std::cout << "[R0] casts the addr to string-type: " << str_serveraddr << std::endl;
 
+        // define rpc-function at the server side
+        std::function<void(const tl::request&, tl::bulk&)> f =
+        [&ser_engine](const tl::request& req, tl::bulk& b) {
+            // get the client’s endpoint (client-addr)
+            tl::endpoint ep = req.get_endpoint();
+
+            // create a buffer of size 6. We initialize segments
+            // and expose the buffer to get a bulk object from it.
+            std::vector<char> v(6);
+            std::vector<std::pair<void*, std::size_t>> segments(1);
+            segments[0].first  = (void*)(&v[0]);
+            segments[0].second = v.size();
+            tl::bulk local = ser_engine.expose(segments, tl::bulk_mode::write_only);
+
+            // The call to the >> operator pulls data from the remote
+            // bulk object b and the local bulk object. 
+            b.on(ep) >> local;
+
+            std::cout << "[R0] SERVER received bulk: ";
+            for(auto c : v) std::cout << c;
+            std::cout << std::endl;
+
+            // Since the local bulk is smaller (6 bytes) than the remote
+            // one (9 bytes), only 6 bytes are pulled. Hence the loop will
+            // print Matthi. It is worth noting that an endpoint is needed
+            // for Thallium to know in which process to find the memory
+            // we are pulling. That’s what bulk::on(endpoint) does.
+        };
+
+        // define the procedure
+        ser_engine.define("do_rdma",f).disable_response();
+
         // use mpi_send to let the client know the server address
         int reciever = 1; // rank 1, send_tag = 0
         MPI_Send(str_serveraddr.c_str(), str_serveraddr.length(), MPI_CHAR, reciever, 0, MPI_COMM_WORLD);
+
 
     } else if (my_rank == 1) {
         // check the client
@@ -138,9 +171,33 @@ int main(int argc, char **argv){
         std::cout << "[R1] got the serv-addr: " << ser_addr << std::endl;
 
         // init the tl-client mode
-        // tl::engine cli_engine("tcp", MARGO_CLIENT_MODE);
-        // tl::remote_procedure remote_do_rdma = cli_engine.define("do_rdma").disable_response();
-        // tl::endpoint ser_endpoint = cli_engine.lookup(ser_addr);
+        tl::engine cli_engine("tcp", MARGO_CLIENT_MODE);
+        tl::remote_procedure remote_do_rdma = cli_engine.define("do_rdma").disable_response();
+        tl::endpoint ser_endpoint = cli_engine.lookup(ser_addr);
+
+        // we define a buffer with the content “Matthieu” (because it’s a string,
+        // there is actually a null-terminating character). We then define
+        // segments as a vector of pairs of void* and std::size_t
+        std::string buffer = "Matthieu";
+        std::vector<std::pair<void*, std::size_t>> segments(1);
+
+        // Each segment (here only one) is characterized by its starting
+        // address in local memory and its size. 
+        segments[0].first  = (void*)(&buffer[0]);
+        segments[0].second = buffer.size()+1;
+        std::cout << "[R1] CLIENT num_characters = " << buffer.size()+1
+                << ", size = " << sizeof(buffer)
+                << std::endl;
+
+        // We call engine::expose to expose the buffer and get a bulk instance from it.
+        // We specify tl::bulk_mode::read_only to indicate that the memory will only be
+        // read by other processes (alternatives are tl::bulk_mode::read_write
+        // and tl::bulk_mode::write_only). 
+        tl::bulk myBulk = cli_engine.expose(segments, tl::bulk_mode::read_only);
+
+        // Finally we send an RPC to the server, passing the bulk object as an argument.
+        // Get back the arrival time at server
+        remote_do_rdma.on(ser_endpoint)(myBulk);
 
         // free the memory allocated by new
         delete[] rec_buf; // because having [size] after new
