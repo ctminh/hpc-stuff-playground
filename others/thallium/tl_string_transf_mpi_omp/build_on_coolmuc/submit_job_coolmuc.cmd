f17c1666de8b0909a7d979793ecd83f96d396b40
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-04-30 14:42:12 +0200

    added build-stuff for coolmuc

---
+++ others/thallium/tl_string_transf_mpi_omp/build_on_coolmuc/submit_job_coolmuc.cmd
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH -J tl_string_transf_mpi_omp
+#SBATCH -o ./logs/tl_string_mpiomp_test_%J.out
+#SBATCH -e ./logs/tl_string_mpiomp_test_%J.err
+#SBATCH -D ./
+#SBATCH --get-user-env
+#SBATCH --clusters=cm2
+#SBATCH --partition=cm2_std
+#SBATCH --qos=cm2_std
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --export=NONE
+#SBATCH --time=00:05:00
+
+module load slurm_setup
+
+module use ~/.modules
+module load local-spack
+module use ~/local_libs/spack/share/spack/modules/linux-sles15-haswell
+module load argobots-1.0-gcc-7.5.0-mio7f7m
+module load boost-1.76.0-gcc-7.5.0-fromrfo
+module load cereal-1.3.0-gcc-7.5.0-jwb3bux
+module load libfabric-1.11.1-gcc-7.5.0-yroqjwu
+module load mercury-2.0.0-gcc-7.5.0-mmuxedi
+module load mochi-abt-io-0.5.1-gcc-7.5.0-qo6dgid
+module load mochi-margo-0.9.1-gcc-7.5.0-uw3savi
+module load mochi-thallium-0.7-gcc-7.5.0-b6d73xq
+## module load hcl-dev
+
+## --------------------------------------------------------------------------
+## -------- Running both tl-server and client in a single mpi-program -------
+echo "mpirun -n ${SLURM_NTASKS} ./tl_string_rdma_transf"
+mpirun -n ${SLURM_NTASKS} ./tl_string_rdma_transf
+
+
