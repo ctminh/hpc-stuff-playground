baffb5d475b6861ffcdacd6e66084c35247d9094
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-05-05 17:14:49 +0200

    check timer for measuring the bandwidth

--- others/thallium/tl_string_transf_mpi_omp/tl_string_rdma_transf.cpp
+++ others/thallium/tl_string_transf_mpi_omp/tl_string_rdma_transf.cpp
@@ -109,7 +109,8 @@ int main(int argc, char **argv){
 
             // Wait for gathering to complete before printing the values received
             MPI_Wait(&gath_request, MPI_STATUS_IGNORE);
-            printf("[R%d] Elapsed-time: %f (s)\n", 0, (iallgather_time_buffer[1]-iallgather_time_buffer[0]));
+            std::cout << "[R0] recv_time: " << recv_time << std::endl;
+            std::cout << "[R0] Elapsed-time: " << (iallgather_time_buffer[1]-iallgather_time_buffer[0]) << std::endl;
         };
 
         // define the procedure
@@ -174,7 +175,8 @@ int main(int argc, char **argv){
 
         // Wait for gathering to complete before printing the values received
         MPI_Wait(&gath_request, MPI_STATUS_IGNORE);
-        printf("[R%d] Elapsed-time: %f (s)\n", my_rank, (iallgather_time_buffer[1]-iallgather_time_buffer[0]));
+        std::cout << "[R1] send_time: " << send_time << std::endl;
+        std::cout << "[R1] Elapsed-time: " << (iallgather_time_buffer[1]-iallgather_time_buffer[0]) << std::endl;
 
         // free the memory allocated by new
         delete[] rec_buf; // because having [size] after new
