bd6f199f3ab9c5b06879a567d056e561245c36b5
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-05-22 11:55:37 +0200

    updated the example running with itac tracer

--- dist-stuff/bcl_examples/task_stealing_queue/run_script_on_laptopnm.sh
+++
@@ -1,1 +0,0 @@
-OMP_NUM_THREADS=3 mpirun -n 2 ./taskStealing 1000 10
---
+++ dist-stuff/bcl_examples/task_stealing_queue/run_with_ITAC_on_laptopnm.sh
@@ -0,0 +1,2 @@
+export VT_LOGFILE_PREFIX=/home/ctminh/projects/visualized_traces/bcl-examples/bcl-task-stealing-queuetest
+OMP_NUM_THREADS=3 mpirun -n 2 -trace ./taskStealing 500 10
