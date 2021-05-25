e80d78efe1cc4580cbfd7253e3b4ec3c211d1899
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-05-25 18:39:16 +0200

    added compile-script for hcl on coolmuc2

--- dist-stuff/hcl_examples/custom_queue_test/build_on_coolmuc_verbs/cmake-script-on-coolmuc2.sh
+++ dist-stuff/hcl_examples/custom_queue_test/build_on_coolmuc_verbs/cmake-script-on-coolmuc2.sh
@@ -1,33 +1,23 @@
 echo "1. Removing old-cmake-files..."
 rm -r CMakeCache.txt  ./CMakeFiles cmake_install.cmake  Makefile
 
-echo "2. Exporting Intel-MPI into LIBRARY_PATH, INCLUDE, etc, ..."
-echo "   (on CoolMUC2, not sure neccessary on SuperMUC-NG)..."
-export INCLUDE=/dss/dsshome1/lrz/sys/spack/staging/20.2.2/opt/x86_64/intel-mpi/2019.8.254-gcc-vyzek4m/compilers_and_libraries_2020.2.254/linux/mpi/intel64/include:$INCLUDE
-export CPATH=/dss/dsshome1/lrz/sys/spack/staging/20.2.2/opt/x86_64/intel-mpi/2019.8.254-gcc-vyzek4m/compilers_and_libraries_2020.2.254/linux/mpi/intel64/include:$CPATH
-export LIBRARY_PATH=/dss/dsshome1/lrz/sys/spack/staging/20.2.2/opt/x86_64/intel-mpi/2019.8.254-gcc-vyzek4m/compilers_and_libraries_2020.2.254/linux/mpi/intel64/lib:$LIBRARY_PATH
-
-echo "3. Loading local-spack (on CoolMUC)..."
+echo "2. Loading Intel OneAPI on CoolMUC2 ..."
+# module load intel-oneapi/2021.1
 module use ~/.modules
-module load local-spack
 module use ~/local_libs/spack/share/spack/modules/linux-sles15-haswell
 
 # load dependencies
-echo "3. Loading hcl, thallium, mercury-rpc dependencies..."
-module load cmake-3.19.5-gcc-7.5.0-bxubtlf
-module load argobots-1.0-gcc-7.5.0-x75podl
-module load boost-1.75.0-gcc-7.5.0-xdru65d
-module load cereal-1.3.0-gcc-7.5.0-jwb3bux
-module load libfabric-1.11.1-gcc-7.5.0-p6j52ik
-# module load mercury-2.0.0-gcc-7.5.0-z55j3mp
-module load mercury-1.0.1-gcc-7.5.0-edeh2o7
-module load mochi-abt-io-0.5.1-gcc-7.5.0-w7nm5r2
-# module load mochi-margo-0.9.1-gcc-7.5.0-n2p7v3n
-module load mochi-margo-0.5-gcc-7.5.0-wqceknu
-# module load mochi-thallium-0.8.4-gcc-7.5.0-u5zn3qg
-# module load mochi-thallium-0.7-gcc-7.5.0-nbeiina
-module load mochi-thallium-0.5.4-gcc-7.5.0-6orqcsa
-module load hcl-dev
+echo "3. Loading thallium-rpc, mercury, margo dependencies..."
+module load hwloc-2.4.1-oneapi-2021.1-rrkvihp
+module load cmake-3.20.1-oneapi-2021.1-enbi76g
+module load argobots-1.0-oneapi-2021.1-67jzgco
+module load boost-1.76.0-oneapi-2021.1-z6jxqo3
+module load cereal-1.3.0-oneapi-2021.1-h45mkqp
+module load libfabric-1.11.1-oneapi-2021.1-ndxraku # this one is built with an updated name of rdma-core on coolmuc
+module load mercury-2.0.0-oneapi-2021.1-gecxu7y # this one is built with an updated name of rdma-core on coolmuc
+module load mochi-abt-io-0.5.1-oneapi-2021.1-byoahez
+module load mochi-margo-0.9.1-oneapi-2021.1-fwjkhal
+module load mochi-thallium-0.7-oneapi-2021.1-ln74gab
 
 # indicate which compiler for C/C++
 echo "4. Setting which C/C++ compiler is used..."
