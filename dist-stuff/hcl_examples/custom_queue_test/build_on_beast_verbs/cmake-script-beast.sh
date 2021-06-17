00223825d8496b61df4c3154c054e9d7c9740ec9
ctminh <minh.thanh.chung@ifi.lmu.de>
2021-06-17 21:22:05 +0200

    updated dependencies-packages for hcl on beast

--- dist-stuff/hcl_examples/custom_queue_test/build_on_beast_verbs/cmake-script-beast.sh
+++ dist-stuff/hcl_examples/custom_queue_test/build_on_beast_verbs/cmake-script-beast.sh
@@ -3,27 +3,22 @@ rm -r CMakeCache.txt  ./CMakeFiles cmake_install.cmake  Makefile
 
 echo "2. Exporting MPICH on BEAST ..."
 module use ~/.module
-module use ~/loc-libs/spack/share/spack/modules/linux-sles15-zen2
-module load mpich-3.3.2-gcc-10.2.1-fyao74s  # without linking fortran
-# module load mpich-3.3.2-gcc-10.2.1-xoqrrzh  # linking fortran but got error on beast
+module use ~/loc-libs/spack/share/spack/lmod/linux-sles15-zen2
+# module load mpich-3.3.2-gcc-10.2.1-nfqpwl7  # without linking fortran, +argobots, -verbs
+module load mpich-3.3.2-gcc-10.2.1-l57k3pr    # without linking fortran, -argobots, +verbs
 
 echo "3. Loading hcl, thallium dependencies, ..."
-module load hwloc-2.4.1-gcc-10.2.1-7svevzl
-module load cmake-3.20.1-gcc-10.2.1-7cjd5mz
+module load hwloc-2.4.1-gcc-10.2.1-bu232a3
+module load cmake-3.20.3-gcc-10.2.1-crj7py7
 module load argobots-1.1-gcc-10.2.1-s3e2vao
-module load boost-1.76.0-gcc-10.2.1-h37ct6b
+module load boost-1.76.0-gcc-10.2.1-hotyooi
 module load cereal-1.3.0-gcc-10.2.1-vd6dtp3
-module load libfabric-1.11.1-gcc-10.2.1-bba3lbj
-module load mercury-2.0.1rc3-gcc-10.2.1-frvg6o6
-module load mochi-abt-io-0.5.1-gcc-10.2.1-rghdmos
-module load mochi-margo-0.9.4-gcc-10.2.1-caqzps7
-module load mochi-thallium-0.7-gcc-10.2.1-i4yovp5 # with libfabric-1.11.1-gcc-10.2.1-bba3lbj enabled sockets
+module load libfabric-1.11.1-gcc-10.2.1-iy4glnm # this one is built with an updated name of rdma-core on beast
+module load mercury-2.0.1rc3-gcc-10.2.1-qlbkvcr # this one is built with an updated name of rdma-core on beast
+module load mochi-abt-io-0.5.1-gcc-10.2.1-fm7cjy2
+module load mochi-margo-0.9.4-gcc-10.2.1-d2oqzan
+module load mochi-thallium-0.7-gcc-10.2.1-sgig2wg
 module load hcl-tl-roce-1.0
-
-# module load libfabric-1.11.1-gcc-10.2.1-7rkzvhv # this one is built with an updated name of rdma-core on beast
-# module load mercury-2.0.1rc3-gcc-10.2.1-565ptkn # this one is built with an updated name of rdma-core on beast
-# module load mochi-margo-0.9.4-gcc-10.2.1-7sqzydv
-# module load mochi-thallium-0.7-gcc-10.2.1-hhkhxqk
 
 # indicate which compiler for C/C++
 echo "4. Setting which C/C++ compiler is used..."
