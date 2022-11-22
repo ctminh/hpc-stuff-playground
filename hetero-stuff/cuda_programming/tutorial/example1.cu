3cee89fc56b035234c7ed81db3195f67ca252dd5
ctminh <minh.thanh.chung@ifi.lmu.de>
2022-11-22 22:22:31 +0100

    updated cuda code

--- hetero-stuff/cuda_programming/tutorial/example1.cu
+++ hetero-stuff/cuda_programming/tutorial/example1.cu
@@ -12,6 +12,14 @@ void add(int n, float *x, float *y){
 // main function
 int main(int argc, char *argv[]){
   int N = 1 << 27; // ~134M elements
+  int blocks = 1;
+  int threads = 1;
+
+  // adjust the blocks and threads
+  if (argc > 1){
+    blocks = atoi(argv[1]);    // adjust block size
+    threads = atoi(argv[2]);   // adjust num. threads/block
+  }
 
   // allocate unified memory which is accessible from CPU or GPU
   float *x, *y;
@@ -27,7 +35,7 @@ int main(int argc, char *argv[]){
   // set timer and run the kernal on GPU side
   clock_t start = clock();
 
-  add<<<1,1>>>(N, x, y);
+  add<<<blocks,threads>>>(N, x, y);
   cudaDeviceSynchronize();
 
   clock_t end = clock();
@@ -38,7 +46,10 @@ int main(int argc, char *argv[]){
   for (int i = 0; i < N; i++){
     max_error = fmax(max_error, fabs(y[i] - 3.0f));
   }
-  printf("N=%d | Elapsed time on GPU: %10.2f ms | max_error=%7.2f\n", N, elapsed_seconds*1000, max_error);
+  printf("---------------------------------------------------\n");
+  printf("num_blocks x num_threads = %d x %d\n", blocks, threads);
+  printf("N=%d | Elapsed time: %10.2f ms | max_error=%7.2f\n", N, elapsed_seconds*1000, max_error);
+  printf("---------------------------------------------------\n");
 
   // free memory
   cudaFree(x);
